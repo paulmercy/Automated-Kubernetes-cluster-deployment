e2d8dbc5a012411edef18e5bf62b553ee3c47ca9
paulmercy <cpaul741@gmail.com>
2023-07-20 08:47:33 +0100

    Load Test

--- post_results.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PR_ID=$(gh pr view --json number -q .number)
-RESULTS=$(cat /path/to/results/file) # replace with the actual location of your results file
-gh pr comment $PR_ID --body "$RESULTS"
